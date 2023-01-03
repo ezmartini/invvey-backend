5567bdb4b52f0d215cf1cae39647fdf097f68abf
ezmartini <em4047@nyu.edu>
2023-01-02 21:48:09 -0500

    connect db

--- server.mjs
+++ server.mjs
@@ -1,10 +1,19 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import mongodb from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const uri = process.env.DB_CNXN;
+
+const client = new mongodb.MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(cors());
 app.use(bodyParser.json());
