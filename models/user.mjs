265caa45d26bb8253efb3c386a4e2c643768fff8
ezmartini <em4047@nyu.edu>
2023-01-03 20:24:14 -0500

    logic for login stratgey

--- models/user.mjs
+++ models/user.mjs
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { hashPassword } from "../utils.mjs";
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
@@ -6,6 +7,13 @@ const userSchema = new mongoose.Schema({
   collections: [{ type: mongoose.Types.ObjectId, ref: "Collection" }],
   products: [{ type: mongoose.Types.ObjectId, ref: "Product" }],
   password: { type: String, required: true },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.password && this.isModified("password")) {
+    this.password = await hashPassword(this.password);
+  }
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
--- routes/users.mjs
+++ routes/users.mjs
@@ -1,13 +1,54 @@
 import express from "express";
-import bcrypt from "bcrypt";
 import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
 
 import Users from "../models/user.mjs";
+import User from "../models/user.mjs";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const router = express.Router();
 
-router.post("/register", (req, res) => {});
+// local strategies
 
-router.login("/login", (req, res) => {});
+passport.use(
+  "local-register",
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password",
+      passReqToCallback: true,
+    },
+    function (req, username, password, done) {
+      Users.findOne({ username: req.body.username }, function (err, user) {
+        if (err) {
+          return done(err);
+        }
+        if (user) {
+          return done(null, false, { message: "Username already exists!" });
+        } else {
+          const newUser = new User({
+            username: username,
+            password: password,
+            businessName: req.body.businessName,
+          });
+          newUser.save(function (err) {
+            if (err) throw err;
+            return done(null, newUser);
+          });
+        }
+      });
+    }
+  )
+);
+
+router.post(
+  "/register",
+  passport.authenticate("local-register"),
+  function (req, res) {
+    // on successful register
+  }
+);
 
 export default router;
